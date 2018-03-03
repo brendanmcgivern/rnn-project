1f8ff5bac78873927c36dd2936754eae09a0373f
Ubuntu <ubuntu@ip-172-31-43-35.us-east-2.compute.internal>
2018-03-03 04:15:48 +0000

    implemented build_part1_RNN and cleaned_text

--- my_answers.py
+++ my_answers.py
@@ -9,9 +9,13 @@ import keras
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
-    # containers for input/output pairs
+    # containers for input/output pairs    
     X = []
     y = []
+
+    # E.g given a time series of length P and window size T = 5, we create P - 5 input/output pairs
+    X = [series[i:window_size + i] for i in range(len(series)-window_size)]
+    y = [series[window_size + i] for i in range(len(series)-window_size)]
 
     # reshape each 
     X = np.asarray(X)
@@ -23,12 +27,19 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size,1)))
+    model.add(Dense(1))
+    
+    return model
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    
+    for c in text:
+        if not c.isalpha() and c not in punctuation:
+            text = text.replace(c,' ')
 
     return text
 
